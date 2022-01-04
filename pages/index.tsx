74585daec015e6c7be8f199de31ea1f7435271d8
Nanai10a <Nanai10a@outlook.jp>
2022-01-05 03:10:12 +0900

    🎨 formatted.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Page: NextPage = () => {
   return (
@@ -14,7 +14,7 @@ const Page: NextPage = () => {
 
       <footer />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
