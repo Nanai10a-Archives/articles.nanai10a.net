48a97c49f9bd5a173ad202428fe0f48797177fa3
Nanai10a <Nanai10a@outlook.jp>
2022-01-06 02:21:08 +0900

    🚧 type defined for `useReducer`.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,17 @@ import type { VFC } from "react";
 import Head from "next/head";
 import "../styles/globals.css";
 
+type State = {
+  title: string;
+  description: string;
+};
+
+type Action =
+  | { type: "changeTitle"; title?: string }
+  | { type: "changeDescription"; description: string };
+
+type Reducer = (state: State, action: Action) => State;
+
 const App: VFC<AppProps<Forwards>> = ({ Component, pageProps }) => {
   return (
     <>
