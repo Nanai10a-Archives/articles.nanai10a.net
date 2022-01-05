fa7b42111ccfe3ad49f1f30a0f06d3f892dc1588
Nanai10a <Nanai10a@outlook.jp>
2022-01-06 02:24:32 +0900

    🚧 make use `useReducer` and specify page's title and description.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from "next/app";
 import type { VFC } from "react";
 
+import { useRef, useReducer } from "react";
 import Head from "next/head";
 import "../styles/globals.css";
+
+const BASE_TITLE = "Nanai10a Articles";
 
 type State = {
   title: string;
@@ -15,11 +18,44 @@ type Action =
 
 type Reducer = (state: State, action: Action) => State;
 
+const reducer: Reducer = (state, action) => {
+  switch (action.type) {
+    case "changeTitle": {
+      const { title } = action;
+      let newTitle = BASE_TITLE;
+
+      if (typeof title === "string") {
+        newTitle = `${title} - ${newTitle}`;
+      }
+
+      return {
+        ...state,
+        title: newTitle,
+      };
+    }
+    case "changeDescription": {
+      const { description } = action;
+
+      return {
+        ...state,
+        description,
+      };
+    }
+  }
+};
+
 const App: VFC<AppProps<Forwards>> = ({ Component, pageProps }) => {
+  const [state, dispatcher] = useReducer<Reducer>(reducer, {
+    title: BASE_TITLE,
+    description: "",
+  });
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.svg" />
+        <title>{state.title}</title>
+        <meta name={"description"} content={state.description} />
       </Head>
     </>
   );
