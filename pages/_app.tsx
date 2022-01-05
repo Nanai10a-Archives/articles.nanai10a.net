03a7198e141119f0243c7a9c68c92af6fc4eff61
Nanai10a <Nanai10a@outlook.jp>
2022-01-06 02:18:40 +0900

    🚧 use `VFC` for declaration.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
+import type { VFC } from "react";
+
+import Head from "next/head";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const App: VFC<AppProps<Forwards>> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+    </>
+  );
 };
 
 export default App;
