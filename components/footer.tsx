11001ad23f9d3741817f6cf0d678e3743915fd17
Nanai10a <Nanai10a@outlook.jp>
2022-01-08 14:33:39 +0900

    🚧 add `Header`, `Footer` components.

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export type Props = Record<string, never>;
+
+const Footer: FC<Props> = () => {
+  return <></>;
+};
+
+export default Footer;
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export type Props = Record<string, never>;
+
+const Header: FC<Props> = () => {
+  return <></>;
+};
+
+export default Header;
