d0e416e5c632d105dbd71bebd890fc1405d53ff7
Nanai10a <Nanai10a@outlook.jp>
2022-01-09 01:41:38 +0900

    🐛 fix conflicting `.children` property and `never` type.

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import type { VFC, PropsWithChildren } from "react";
 import Footer from "./footer";
 import Header from "./header";
 
-export type Props = PropsWithChildren<Record<string, never>>;
+export type Props = PropsWithChildren<Record<string, unknown>>;
 
 const Layout: VFC<Props> = ({ children }) => {
   return (
