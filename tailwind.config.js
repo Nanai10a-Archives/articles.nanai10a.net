3514f619e07cd0b7eb6c98c706ba886f2566a871
Nanai10a <Nanai10a@outlook.jp>
2022-03-19 02:52:29 +0900

    🔧 🎨 define project-specified colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#567097",
+
+        // {light,dark} shades color
+        daccent: "#5c6c96",
+        laccent: "#89a0a6",
+
+        // {light,dark} accent color
+        lshades: "#ebeceb",
+        dshades: "#1e2333",
+
+        // color semantics
+        primary: "#566f99",
+        info: "#1d2b52",
+        success: "#4f9c66",
+        warning: "#cc8c2e",
+        danger: "#f44336",
+      },
+    },
   },
   plugins: [],
 };
