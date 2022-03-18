e15cf90dd3fb79e8c5b655f6d4273d58b02b5cc6
Nanai10a <Nanai10a@outlook.jp>
2022-03-19 04:25:30 +0900

    🚚 change to configuration file as `.mjs` extension.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const config = {
   images: { formats: ["image/avif", "image/webp"] },
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   swcMinify: true,
 };
+
+export default config;
