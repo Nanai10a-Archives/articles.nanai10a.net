1bb625373d48a3bef31a1f4d0c8cdeb81b42e9bd
Nanai10a <Nanai10a@outlook.jp>
2022-03-19 02:51:36 +0900

    🔧 enable "image/avif" compressing supports.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: { formats: ["image/avif", "image/webp"] },
   reactStrictMode: true,
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   swcMinify: true,
